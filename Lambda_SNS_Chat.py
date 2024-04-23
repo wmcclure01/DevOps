e7364fe4fec607896e1a81113d3c1193ec312e7a
Wayne McCLure <89033938+wmcclure01@users.noreply.github.com>
2024-04-23 14:20:40 -0400

    Update Lambda_SNS_Chat.py

--- Lambda_SNS_Chat.py
+++ Lambda_SNS_Chat.py
@@ -2,7 +2,7 @@ from httplib2 import Http
 from json import dumps
 
 def lambda_handler(event, context):
-    url = "https://chat.googleapis.com/v1/spaces/AAAAqYJ0x_Q/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=-tn2I6rQdiBTv7QCwUMQGrE9mfsajQq8FY2KplcH47g%3D"
+    url = "https://chat.googleapis.com/v1/spaces/<HOOK>"
     bot_message = {'text' : event['Records'][0]['Sns']['Message']}
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
     http_obj = Http()
