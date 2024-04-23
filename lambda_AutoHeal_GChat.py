44a256271cd6e193928ed89e66083b834795b3ad
Wayne McCLure <89033938+wmcclure01@users.noreply.github.com>
2024-04-23 14:22:48 -0400

    Update lambda_AutoHeal_GChat.py

--- lambda_AutoHeal_GChat.py
+++ lambda_AutoHeal_GChat.py
@@ -6,7 +6,7 @@ http = urllib3.PoolManager()
 
 
 def lambda_handler(event, context):
-    url = "https://chat.googleapis.com/v1/spaces/AAAAqYJ0x_Q/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=-tn2I6rQdiBTv7QCwUMQGrE9mfsajQq8FY2KplcH47g%3D"
+    url = "https://chat.googleapis.com/v1/spaces/AAAAqYJ0x_Q/messages?key=<HOOK>"
     msg = {
          "text": event["Records"][0]["Sns"]["Message"],
             }
